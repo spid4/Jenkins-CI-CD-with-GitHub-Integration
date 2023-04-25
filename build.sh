06d77452270da6e9cb8e5f7655e09952c08c6437
mikhil <mikhilpasricha52@gmail.com>
2023-04-25 13:00:55 +0530

    dockerfile

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#Installing node 16 on ubuntu 
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt -y install nodejs
+node v
+
+#Install pm2 pacakege
 sudo npm install
 sudo npm i -g pm2
+
+#Starting our nodejs app 
 sudo pm2 start app.js --name "todo"
