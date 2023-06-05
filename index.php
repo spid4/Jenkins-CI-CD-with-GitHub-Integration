cd061227a18e2a3e59617c41f1889d05b7ee83c1
mikhil <mikhilpasricha52@gmail.com>
2023-06-05 14:19:44 +0530

    html

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample PHP File</title>
+</head>
+<body>
+    <h1>Welcome to My PHP File</h1>
+
+    <?php
+        // PHP code goes here
+        $name = "John Doe";
+        $age = 30;
+        echo "<p>My name is mikhil and this is a sample php app.</p>";
+    ?>
+
+    <p>Thank you for visiting!</p>
+</body>
+</html>
